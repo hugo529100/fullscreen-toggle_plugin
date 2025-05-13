7e48f5fc33214b8221cf8eb8ab9e8b22e5f05e78
Hug3O <131569471+hugo529100@users.noreply.github.com>
2025-05-13 15:13:04 +0800

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -8,6 +8,7 @@
     }
   }
 
+  // 加入 options menu
   HFS.onEvent('appendMenuBar', () => {
     return HFS.h(HFS.Btn, {
       icon: '⛶',
@@ -15,4 +16,22 @@
       onClick: toggleFullscreen
     })
   })
+
+  // 插入到 preview 右上角
+  const interval = setInterval(() => {
+    const controls = document.querySelector('.file-show .bar .controls')
+    const closeBtn = controls?.querySelector('button[title="Close"]')
+    const exists = controls?.querySelector('.fullscreen-toggle')
+
+    if (controls && closeBtn && !exists) {
+      const btn = document.createElement('button')
+      btn.className = 'fullscreen-toggle icon-button'
+      btn.title = 'Toggle Fullscreen'
+      btn.innerHTML = '<span aria-hidden="true">⛶</span>'
+
+      btn.onclick = toggleFullscreen
+
+      controls.insertBefore(btn, closeBtn)
+    }
+  }, 500)
 }
